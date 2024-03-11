4ebac71b0754a598f6c5ae71c57389a43f7e6bba
Myke Cameron <mykecameron@gmail.com>
2024-03-11 14:03:26 -0400

    get rid of n+1 query

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,6 +1,6 @@
 class ContactsController < ApplicationController
   def index
     #TODO consider adding a composite index on first_name and last_name
-    @contacts = Contact.all.order(:first_name, :last_name)
+    @contacts = Contact.include(:patient).all.order(:first_name, :last_name)
   end
 end
