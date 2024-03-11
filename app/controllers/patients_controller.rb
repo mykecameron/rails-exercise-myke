13650bd147c40eeab3ff5dc7014e4b2a17b62f1e
Myke Cameron <mykecameron@gmail.com>
2024-03-11 13:43:42 -0400

    initial implementation of patient creation
    could use some refactoring and maybe more rebust tests

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -5,6 +5,24 @@ class PatientsController < ApplicationController
     @patients = Patient.all.order(:first_name, :last_name)
   end
 
+  def create
+    contact = Contact.find(params[:contact_id])
+
+    ApplicationRecord.transaction do
+      patient = contact.create_patient!({
+        first_name: contact.first_name,
+        last_name: contact.last_name,
+        avatar_url: Contact::DEFAULT_AVATAR_URL,
+      })
+      contact.save!
+  
+      flash[:info] = "Created patient '#{patient.first_name} #{patient.last_name}' with Sicklie"
+    end
+
+
+    redirect_to controller: :contacts, action: :index
+  end
+
   def sync
     @patient = Patient.find(params[:id])
 
--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,3 +1,5 @@
 class Contact < ApplicationRecord
+  DEFAULT_AVATAR_URL="https://robohash.org/quitotamnon.png?size=300x300&set=set1"
+
   belongs_to :patient, optional: true
 end
